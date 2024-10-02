1586c2a5051e87fc700f130216ca95ecd4c794cb
nihat-js <abdullazadenihat@gmail.com>
2024-10-02 17:38:26 +0400

    few changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+
+
+      <Link href="/favorites"> Favorites </Link>
+      <Link href="/about"> About </Link>
+
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
