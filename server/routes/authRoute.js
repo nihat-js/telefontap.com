5d5e8da929d2c8449b7f70e39bb4ce60f2010b25
Nihat <abdullazadenihat@gmail.com>
2024-10-08 11:39:49 +0400

    browser agent added

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -3,6 +3,9 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const knex = require("../db/connection"); // Adjust the path to your Knex setup
 const { register } = require("module");
+const { hashPassword } = require("../utils/password")
+const crypto = require('crypto');
+
 
 const router = express.Router();
 
@@ -16,7 +19,7 @@ async function register(req, res) {
   registerViaEmail(req, res)
 }
 async function registerViaEmail(req, res) {
-  const { email, password } = req.body
+  const { name, email, password } = req.body
   const existingUser = await prisma.user.findUnique({
     where: { email: email }
   })
@@ -26,7 +29,11 @@ async function registerViaEmail(req, res) {
 
   const hashedPassword = await hashPassword(password)
   const newUser = await prisma.user.create({
-    data: { email: email, password: hashedPassword }
+    data: {
+      name,
+      email: email,
+      password: hashedPassword
+    }
   })
   res.status(201).json({ message: 'User registered successfully' });
 }
@@ -39,15 +46,20 @@ async function login(req, res) {
     return res.status(401).json({ message: 'Invalid username or password' });
   }
   // Create a session token
-  // const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" })
-  // await prisma.session.create({
-  //   data: {
-  //     token,
-  //     userId: user.id
-  //   }
-  // })
+  // const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1minute" })
+  const token = crypto.randomBytes(32).toString("hex")
+  const ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const browserInfo = parseUserAgent(req.headers['user-agent'])
+  await prisma.session.create({
+    data: {
+      token,
+      ipAddress,
+      userId: user.id,
+      expiresAt: 
+    }
+  })
 
-  // res.json({ message: "Login successfull", token },)
+  res.json({ message: "Login successfull", token },)
   // })
 }
 
---
+++ server/utils/parseUserAgent.js
@@ -0,0 +1,26 @@
+function parseUserAgent(userAgent) {
+  let browserName = "Unknown Browser";
+  let fullVersion = "Unknown Version";
+
+  // Check for various browser signatures in the User-Agent string
+  const isChrome = userAgent.indexOf("Chrome") > -1;
+  const isFirefox = userAgent.indexOf("Firefox") > -1;
+  const isSafari = userAgent.indexOf("Safari") > -1 && !isChrome;
+  const isIE = userAgent.indexOf("MSIE") > -1 || userAgent.indexOf("Trident") > -1;
+
+  if (isChrome) {
+    browserName = "Chrome";
+    fullVersion = userAgent.split("Chrome/")[1].split(" ")[0];
+  } else if (isFirefox) {
+    browserName = "Firefox";
+    fullVersion = userAgent.split("Firefox/")[1];
+  } else if (isSafari) {
+    browserName = "Safari";
+    fullVersion = userAgent.split("Version/")[1].split(" ")[0];
+  } else if (isIE) {
+    browserName = "Internet Explorer";
+    fullVersion = userAgent.split("MSIE ")[1].split(";")[0] || userAgent.split("rv:")[1].split(")")[0];
+  }
+
+  return `${browserName} ${fullVersion}`;
+}
