4e61b5d51fe669d3fb4b18fb1c3a01efbae75c6b
Nihat <abdullazadenihat@gmail.com>
2024-10-08 10:31:55 +0400

    changes in authRoute

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,11 +2,19 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const knex = require("../db/connection"); // Adjust the path to your Knex setup
+const { register } = require("module");
 
 const router = express.Router();
 
 
+router.post("register", register)
+router.post("login", login)
+router.post("logout", logout)
 
+async function register(req, res) {
+  // email ? registerViaEmail(req, res) : registerViaPhoneNumber(req, res)
+  registerViaEmail(req, res)
+}
 async function registerViaEmail(req, res) {
   const { email, password } = req.body
   const existingUser = await prisma.user.findUnique({
@@ -24,37 +32,32 @@ async function registerViaEmail(req, res) {
 }
 
 
-
-router.post("register", async function (req, res) {
-  // email ? registerViaEmail(req, res) : registerViaPhoneNumber(req, res)
-  registerViaEmail(req, res)
-})
-
-router.post("login", async function (req, res) {
+async function login(req, res) {
   const { email, password } = req.body
   const user = await prisma.user.findUnique({ where: { username } });
   if (!user || !(await verifyPassword(password, user.password))) {
     return res.status(401).json({ message: 'Invalid username or password' });
   }
-
   // Create a session token
-  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" })
-  await prisma.session.create({
-    data: {
-      token,
-      userId: user.id
-    }
-  })
+  // const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "1h" })
+  // await prisma.session.create({
+  //   data: {
+  //     token,
+  //     userId: user.id
+  //   }
+  // })
 
-  res.json({ message: "Login successfull", token },)
-})
+  // res.json({ message: "Login successfull", token },)
+  // })
+}
 
-router.post("logout", async (req, res) => {
+
+async function logout(req, res) {
   await prisma.session.deleteMany({
     where: { userId: req.user.userId }
   })
   res.json({ message: "Logged out successfully" })
-})
+}
 
 
 
