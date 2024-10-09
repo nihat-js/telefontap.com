9734ca8a870fb4fb37d95429d4c243f6dc17bf75
Nihat <abdullazadenihat@gmail.com>
2024-10-10 02:04:05 +0400

    user repository added

--- server/app.js
+++ server/app.js
@@ -20,9 +20,7 @@ const logger = require('morgan');
 // const nodemailer = require("nodemailer")
 
 
-const io = new Server({
-
-})
+const io = require("socket.io")(require("http").createServer())
 
 app.use(express.json())
 app.use(logger('dev'));
--- server/services/SlackService.js
+++ server/services/SlackService.js
@@ -1,17 +1,51 @@
 const axios = require('axios');
-const config = require('../config');
+// const config = require('../config');
+// 
+class SlackService {
+  constructor() {
 
-const sendSlackNotification = async (message) => {
-  try {
-    await axios.post(config.slackWebhookUrl, {
-      text: message,
-    });
-    console.log('Notification sent to Slack:', message);
-  } catch (error) {
-    console.error('Error sending notification to Slack:', error);
+    this.webhookUrl = process.SLACK_WEBHOOK_URL;
   }
-};
 
-module.exports = {
-  sendSlackNotification,
-};
+  async sendNotification(message) {
+    try {
+      const response = await axios.post(this.webhookUrl, { text: message });
+      console.log('Notification sent to Slack:', response.data);
+    } catch (error) {
+      console.error('Error sending notification to Slack:', error);
+      throw new Error('Slack notification failed');
+    }
+  }
+
+  // You can add more methods as needed, for example:
+  async sendFormattedNotification(title, body) {
+    const message = `*${title}*\n${body}`;
+    await this.sendNotification(message);
+  }
+
+  async sendAttachmentNotification(title, text, attachmentUrl) {
+    const message = {
+      text: title,
+      attachments: [
+        {
+          text: text,
+          image_url: attachmentUrl,
+          fallback: 'Image not available',
+        },
+      ],
+    };
+    try {
+      await axios.post(this.webhookUrl, message);
+      console.log('Attachment notification sent to Slack');
+    } catch (error) {
+      console.error('Error sending attachment notification to Slack:', error);
+      throw new Error('Slack attachment notification failed');
+    }
+  }
+}
+
+// Exporting an instance of the SlackService
+// const slackServiceInstance = new SlackService(config.slackWebhookUrl);
+const instance = new SlackService()
+instance.sendNotification("bas")
+// module.exports = slackServiceInstance;
