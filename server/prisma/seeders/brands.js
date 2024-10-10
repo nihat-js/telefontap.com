de65512787223728c7615ebde56c970ccf6a18fc
Nihat <abdullazadenihat@gmail.com>
2024-10-10 17:12:27 +0400

    few changes

--- server/prisma/seeders/brands.js
+++ server/prisma/seeders/brands.js
@@ -4,7 +4,6 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-prisma.
 
 async function main() {
   // Create example users
---
+++ server/prisma/seeders/properties.js
@@ -0,0 +1,14 @@
+
+const cities = require("../../../data/cities.json")
+const countries = require("../../../data/countries.json")
+const brands = require("../../../data/brands.json")
+const phoneSpecs = require("../../../data/phoneSpecs.json")
+// const phoneSpecs = require("../../../data/phoneSpecs.json")
+
+
+const prisma = require("../../config/db.js")
+
+
+prisma.cities.u
+
+console.log(cities)
