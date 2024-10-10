0dd65b4a0e0aa73576385c9a7a131f81fa883a98
Nihat <abdullazadenihat@gmail.com>
2024-10-10 16:58:05 +0400

    propertiesRouteAdded

---
+++ server/routes/propertiesRoute.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const prisma = require("../config/db")
+
+const router = express.Router()
+
+
+router.get("/cities", getCities)
+router.get("/countries", getCountries)
+router.get("/brand", getBrands)
+router.get("/phone/:modelName", getPhoneModelsByBrand)
+router.get("/phone/:modelName/spces ", getPhoneModelSpecs)
+
+
+router.get("/phone-models/:modelName", function (req, res) {
+  const brand = req.body
+  await prisma.phoneSpec.findMany({
+    select: {
+      model
+    },
+    where: {
+      brand: 
+  }
+  })
+})
+
+router.get("/phone-models/model", function (req, res) {
+
+})
